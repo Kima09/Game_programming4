e28afaeaf42dba3ffb77e5a3b840fd3f76a34a9f
Kima09 <hkdavidl666@gmail.com>
2025-10-16 19:40:44 +0900

    (2025.10.16) 이중 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,25 +2,27 @@
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 class List
 {
 private:
     struct Node
     {
         T data;
+        Node* previous;
         Node* next;
     };
 
     int size;
-
     Node* head;
+    Node* tail;
 
 public:
     List()
     {
         size = 0;
         head = nullptr;
+        tail = nullptr;
     }
 
     void push_front(T data)
@@ -31,42 +33,12 @@ public:
         {
             head = newNode;
             newNode->next = nullptr;
+            newNode->previous = nullptr;
         }
         else
         {
             newNode->next = head;
-            head = newNode;
-        }
-        size++;
-    }
-
-    void pop_front()
-    {
-        if (head == nullptr)
-        {
-            cout << "linked list is empty" << endl;
-        }
-        else
-        {
-            Node* deleteNode = head;
-            head = deleteNode->next;
-            delete deleteNode;
-            size--;
-        }
-    }
-
-    void push_back(T data)
-    {
-        Node* newNode = new Node;
-        newNode->data = data;
-        if (head == nullptr)
-        {
-            head = newNode;
-            newNode->next = nullptr;
-        }
-        else
-        {
-            newNode->next = head;
+            head->previous = newNode;
             head = newNode;
         }
         size++;
@@ -77,14 +49,7 @@ public:
 int main()
 {
     List<int> list;
-
-    list.push_front(10);
-    list.push_front(5);
-
-    list.pop_front();
-    list.pop_front();
-    list.pop_front();
+    
 
     return 0;
-    
 }
